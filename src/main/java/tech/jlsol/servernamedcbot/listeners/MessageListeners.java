b45ff54ea2f1826bbde2e0782d1352a3f881d9b9
johanneslosch <hi@johanneslosch.com>
2020-12-19 16:16:37 +0100

    :sparkles: feature request command - safe in file

--- src/main/java/tech/jlsol/servernamedcbot/listeners/MessageListeners.java
+++ src/main/java/tech/jlsol/servernamedcbot/listeners/MessageListeners.java
@@ -3,6 +3,7 @@ package tech.jlsol.servernamedcbot.listeners;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import tech.jlsol.servernamedcbot.util.Config;
+import tech.jlsol.servernamedcbot.util.WriteFile;
 
 import javax.annotation.Nonnull;
 
@@ -10,12 +11,14 @@ public class MessageListeners extends ListenerAdapter {
     private String getConfig(String key){
         return Config.readConfig("data", "settings", key);
     }
-    //TODO: get message & so on!
+    private void writeToFile(String content){
+        WriteFile.writer("data", "msg", "txt", content);
+    }
     public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
         if(!event.getAuthor().isBot()){
             if(event.getChannel().getName().equals(getConfig("featureRQ-TC"))){
                 if(event.getMessage().getContentRaw().toLowerCase().startsWith(getConfig("prefix"))){
-                    event.getChannel().sendMessage(":check:").queue();
+                    writeToFile(event.getMessage().getContentDisplay().substring(getConfig("prefix").length()));
                 }
             }else{
                 System.out.println("Message received from " + event.getAuthor().getName());
