63b176e23b0858d74f0fb94604cedd844d0ee48c
johanneslosch <hi@johanneslosch.com>
2021-01-06 02:41:12 +0100

    SQL

---
+++ src/main/java/tech/jlsol/servernamedcbot/listeners/logger/Messages.java
@@ -0,0 +1,58 @@
+package tech.jlsol.servernamedcbot.listeners.logger;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageDeleteEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageEmbedEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageUpdateEvent;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionRemoveAllEvent;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionRemoveEmoteEvent;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionRemoveEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import tech.jlsol.servernamedcbot.util.Logger;
+import tech.jlsol.servernamedcbot.util.SQLHandler;
+
+import javax.annotation.Nonnull;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Messages extends ListenerAdapter {
+    String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(Calendar.getInstance().getTime());
+
+    public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
+        try {
+            SQLHandler.MySQLUseDataManager.setMessageReceive(timeStamp, event.getChannel().getId(),
+                    event.getMessage().getContentDisplay().length(), event.getAuthor().isBot(), event.getAuthor().getJDA().getRoles().size(), event.getGuild().getId());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            Logger.error(throwables.getMessage());
+        }
+    }
+    public void onGuildMessageUpdate(@Nonnull GuildMessageUpdateEvent event) {
+        try {
+            SQLHandler.MySQLUseDataManager.setMessageReceive(timeStamp, event.getChannel().getId(),
+                    event.getMessage().getContentDisplay().length(), event.getAuthor().isBot(), event.getAuthor().getJDA().getRoles().size(), event.getGuild().getId());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            Logger.error(throwables.getMessage());
+        }
+    }
+    public void onGuildMessageReactionAdd(@Nonnull GuildMessageReactionAddEvent event) {
+        try {
+            SQLHandler.MySQLUseDataManager.setMessageReact(timeStamp, event.getChannel().getId(), event.getUser().isBot(), event.getUser().getJDA().getRoles().size(), event.getGuild().getId(), event.getReaction().getReactionEmote().getEmoji());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            Logger.error(throwables.getMessage());
+        }
+    }
+    public void onGuildMessageReactionRemove(@Nonnull GuildMessageReactionRemoveEvent event) {
+        try {
+            SQLHandler.MySQLUseDataManager.setMessageReact(timeStamp, event.getChannel().getId(), event.getUser().isBot(), event.getUser().getJDA().getRoles().size(), event.getGuild().getId(), event.getReaction().getReactionEmote().getEmoji());
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            Logger.error(throwables.getMessage());
+        }
+    }
+
+}
